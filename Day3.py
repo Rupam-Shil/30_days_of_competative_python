bf7024146628a14ffaf5a93af8eed9b4c4c10e61
Rupam Shil <rupamshil2911@gmail.com>
2021-02-08 12:59:37 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,9 @@
+# A Chef was Cooking something special when required exact 200ml water but
+# he doesnot have 200ml cup he has a 500ml cup and a 300ml cup but that dish
+# required exactly 200ml water.
+# Can you solve this problem in python
+
+bigCup = 500
+smallCup = 300
+requiredCup = bigCup - smallCup
+print(requiredCup)
